951e6427e0ffbf901c8c38b96cffcde90ead9bbb
Natalia Manrique <nmanrique@gmail.com>
2023-01-19 19:42:32 -0500

    first commit

---
+++ data/Products.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    name: "Short Tie Dye Lila",
+    image: [
+      "https://res.cloudinary.com/dmkbissq5/image/upload/v1669779011/outfitness/shorttiedye_morado_2_eq070z.jpg",
+      "https://res.cloudinary.com/dmkbissq5/image/upload/v1669779011/outfitness/shorttiedye_morado_1_qpk3rt.jpg"
+    ],
+    description:
+      "Shorts hechos para que entrenes comoda.",
+    price: 75000,
+    countInStock: 0,
+    rating: 5,
+    numReviews: 0,
+  }
+];
+
+export default products;
